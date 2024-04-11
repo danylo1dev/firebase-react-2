4315f74c09b51f8b33905d68783406ad1f9b3912
unknown <39314465+BigBlueDraco@users.noreply.github.com>
2024-04-11 18:00:19 +0300

    init

---
+++ src/utils/fbase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = { 
+    apiKey: "AIzaSyAX21k1oCahCYl83v-Mf6rqnS39pQCj5As",
+    authDomain: "xnbeats-361a9.firebaseapp.com",
+    databaseURL: "https://xnbeats-361a9.firebaseio.com",
+    projectId: "xnbeats-361a9",
+    storageBucket: "xnbeats-361a9.appspot.com",
+    messagingSenderId: "447122881394",
+    appId: "1:447122881394:web:d671ab422736e30d997beb",
+    measurementId: "G-CN4KSQHKC2"
+}
+
+const fBaseApp = firebase.initializeApp(config);
+
+// DB
+export const DB = fBaseApp.firestore();
+export const { Timestamp } = firebase.firestore;
+export const usersCollection = DB.collection('users');
