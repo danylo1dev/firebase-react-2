8b4b0139ae4b9e1b69a3d7ce98c041584440a6e8
unknown <39314465+BigBlueDraco@users.noreply.github.com>
2024-04-14 18:35:55 +0300

    add: hosting script

--- src/utils/fbase.js
+++ src/utils/fbase.js
@@ -1,23 +1,21 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
-const config = { 
-    apiKey: "AIzaSyAX21k1oCahCYl83v-Mf6rqnS39pQCj5As",
-    authDomain: "xnbeats-361a9.firebaseapp.com",
-    databaseURL: "https://xnbeats-361a9.firebaseio.com",
-    projectId: "xnbeats-361a9",
-    storageBucket: "xnbeats-361a9.appspot.com",
-    messagingSenderId: "447122881394",
-    appId: "1:447122881394:web:d671ab422736e30d997beb",
-    measurementId: "G-CN4KSQHKC2"
-}
+const config = {
+  apiKey: "AIzaSyAMRHdDU0s2Bo5lXwvcMDrBH1eXb_A7-PE",
+  authDomain: "xnbeats-app.firebaseapp.com",
+  projectId: "xnbeats-app",
+  storageBucket: "xnbeats-app.appspot.com",
+  messagingSenderId: "1008294026302",
+  appId: "1:1008294026302:web:d9f748189378b889ca056f",
+};
 
 const fBaseApp = firebase.initializeApp(config);
 
 // DB
 export const DB = fBaseApp.firestore();
 export const { Timestamp } = firebase.firestore;
-export const usersCollection = DB.collection('users');
-export const reviewsCollection = DB.collection('reviews');
-export const messagesCollection = DB.collection('messages');
+export const usersCollection = DB.collection("users");
+export const reviewsCollection = DB.collection("reviews");
+export const messagesCollection = DB.collection("messages");
